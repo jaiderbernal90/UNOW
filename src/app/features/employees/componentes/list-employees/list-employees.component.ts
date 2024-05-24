db29e1606b33be4c1143718dcc299961e92b4657
jaiderbernal90 <64909036+jaiderbernal90@users.noreply.github.com>
2024-05-24 10:07:33 -0500

    Feature: Employee

---
+++ src/app/features/employees/componentes/list-employees/list-employees.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-employees',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-employees.component.html',
+  styleUrl: './list-employees.component.scss'
+})
+export class ListEmployeesComponent {
+
+}
